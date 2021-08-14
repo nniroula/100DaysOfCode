954d5048463e90add352c55ce30938a07c084ffc
Nabin <nkbhaie2017@gmail.com>
2021-08-13 22:09:08 -0600

    created a function that accepts a user input and returns it back

---
+++ functions.py
@@ -0,0 +1,8 @@
+#!/Users/nabinniroula/anaconda3/bin/python
+
+# Functions without parameters:
+def name():
+    name = input("what is your name? ")
+    print(f"Hello, your name is {name}.")
+    return f"Thank you {name}."
+print(name())
