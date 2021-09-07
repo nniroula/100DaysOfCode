bec50311bd0d25ad56fc80404252f2f2fdec5b83
Nabin <nkbhaie2017@gmail.com>
2021-09-07 07:03:49 -0600

    add __init__() without any parameters to use get and set methods

--- python_oop.py
+++ python_oop.py
@@ -15,11 +15,12 @@ class Dog:
         print("This is __init__ method. ")
         self.bark()
         self.legs()
+
     def bark(self):
-        print("bark")
+        return "bark"
     
     def legs(self):
-        print("It has four legs")
+        return "It has four legs"
 d1 = Dog()
 # print(d1.bark())
 # print(d1.legs())
@@ -42,4 +43,25 @@ print(f"The age of {dog2.name} is {dog2.age}.")
 # second way of assigning a value to an objct is as follows
 dog1.name = "Sakira"
 print(f"The updated name of the dog 1 object is {dog1.name}.")
+
+# If there are thousands of objects, the above idea of setting and getting object attributes do not work. So, use get and set methods
+
+# eg 2
+class Dog2:
+    def __init__(self):
+        # self.name = name
+        # self.age = age
+        # if it says that name is not defined, set it to "None"  for initialization
+        self.name = "None"
+        self.age = "None"
+
+    def set_name(self, new_name):
+        self.name = new_name
+    
+    def get_name(self):
+        return self.name
+
+dog_1 = Dog2()
+dog_1.set_name("Eunta")
+print(f"The name of the dog 1 object using set() is {dog_1.get_name()}.")
 
