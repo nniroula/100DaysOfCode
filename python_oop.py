38770d025d0028bbd512cee54cab2848ad1ad9f3
Nabin <nkbhaie2017@gmail.com>
2021-09-07 06:31:22 -0600

    add more oop concepts

--- python_oop.py
+++ python_oop.py
@@ -2,7 +2,18 @@
 
 """ Classic example of python OOP Concept. """
 
+# To check the type of data type
 x = 1
-print(type(x))
+# print(type(x))
+# print(type("hello"))
 
-print(type("hello"))
+class Dog:
+    def bark(self):
+        print("bark")
+    
+    def legs(self):
+        print("It has four legs")
+d1 = Dog()
+# print(d1.bark())
+# print(d1.legs())
+# print(type(d1))
