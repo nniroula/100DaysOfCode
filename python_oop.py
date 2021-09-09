f8f047457fc5146d0721eead82883d358e3ae548
Nabin <nkbhaie2017@gmail.com>
2021-09-08 22:45:34 -0600

    create a choose your own adventure game

--- python_oop.py
+++ python_oop.py
@@ -2,6 +2,7 @@
 
 """ Classic example of python OOP Concept. """
 
+"""
 # To check the type of data type
 x = 1
 # print(type(x))
@@ -74,4 +75,46 @@ dog_1.set_name("Eunta")
 dog_1.set_age(10)
 print(f"The name of the dog 1 object using set() is {dog_1.get_name()}.")
 print(f"The age of the dog object using set and get method is {dog_1.get_age()}.")
+
+"""
+# eg 3 student and course classes
+
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade  # 1 - 100 numeric values only
+
+    def get_grade(self):
+        return self.grade
+
+class Course:
+    def __init__(self, name_of_course, max_std):
+        self.name_of_course = name_of_course
+        self.max_std = max_std
+        self.enrolled_std = []
+    
+    def add_std(self, student):
+        if len(self.enrolled_std) < self.max_std:
+            self.enrolled_std.append(student)
+            return True
+        else:
+            return False
+
+    def get_std(self):
+        # return [std for std in self.enrolled_std]
+        for std in self.enrolled_std:
+            return std
+
+student1 = Student("Nabin", 34, 90)
+student2 = Student("Prabha", 1, 100)
+
+# add student to the course, add student1 and student2 to the course
+course1 = Course("Computer Science", 2)
+course1.add_std(student1)
+
+print(course1.add_std(student1))
+# print(course1.enrolled_std)
+print(course1.get_std())
+
 
