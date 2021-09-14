50ae51b4ce30a63133899d39f53354d92e246274
Nabin <nkbhaie2017@gmail.com>
2021-09-13 19:50:58 -0600

    add strip() function

--- Crash_Course/built_in_func.py
+++ Crash_Course/built_in_func.py
@@ -1,6 +1,6 @@
 name = "Nabin Niroula"
 my_name = name.title() # title() converts the first letter of the each word to upper case.
-print(f"This is due to title() function: {my_name}.")
+# print(f"This is due to title() function: {my_name}.")
 
 #print(f"this is due to lowr() function in python, {name.lower()}.")
 #print(f"This is due to upper() funciton in pythonn, {name.upper()}.")
@@ -12,12 +12,39 @@ new_lines_and_tabs = ("Languages are: \n\tPython\n\tJavascript\n\tHTML\n\tCSS\n\
 
 """ striping white spaces in strings. White spaces include tabs, spaces, and newline characters. """
 
-# remove white spaces to the right with rstrip() function
-college2 = "Univerisyt of Colorado, Denver "
-remove_space = college2.rstrip()
-print(remove_space)
+#1 remove white spaces to the right with rstrip() function
+college3 = "Univerisyt of Colorado, Denver "
+remove_space = college3.rstrip()
+#print(remove_space)
 
-# remove space at the beginning of the string with lstrip() method
-college1 = " MSU Denver."
-remove_left_space = college1.lstrip()
-print(f"After the space is removed, {remove_left_space}")
+# 2 remove space at the beginning of the string with lstrip() method
+college2 = " MSU Denver."
+remove_left_space = college2.lstrip()
+#print(f"After the space is removed, {remove_left_space}")
+
+#3 remove spaces on both sides using strip() method
+developer_school = " Springboard "
+remove_space_both_side = developer_school.strip()
+#print(f"After the space is removed on both sides, {developer_school}.")
+
+# does strip() has side effect if used ot remove only left or right end space
+college1 = " Comminity Colelge of Aurora"
+#print("Afer space at beginning is removed is" + college1.strip())
+#print("The space after ',' is not due to strip() method to removoe space, " + college1.strip())
+
+#1 personal message
+name = "Pabha Niroula" # in titlecase, lowercase, and uppercase
+# print(f"Hello {name.title()}, welcome to Python programming. ")
+# print(name.lower())
+# print(name.upper())
+
+#2 striping white spaces like spaces, tabs, and newline characters with lstirp(), rstrip(), and strip() methods
+
+name1 = "\n\tPabira Niroula\n Nabisha Niroula \tPrinsu Niroula"
+# name2 = "Prinsu Niroula"
+# Now remove all the white spaces
+print(f"Before white spaces are removed: {name1}")
+print(f"After white spaces are removed: {name1.strip()}") # only removes white spaces from the beggin and the end, not from the middle
+
+
+
