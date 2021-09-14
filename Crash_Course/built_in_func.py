0a569d66c9aee670717c8c81d2508b89de2fc652
Nabin <nkbhaie2017@gmail.com>
2021-09-13 18:33:26 -0600

    add built in function title()

---
+++ Crash_Course/built_in_func.py
@@ -0,0 +1,6 @@
+name = "Nabin Niroula"
+my_name = name.title() # title() converts the first letter of the each word to upper case.
+print(f"This is due to title() function: {my_name}.")
+
+print(f"this is due to lowr() function in python, {name.lower()}.")
+print(f"This is due to upper() funciton in pythonn, {name.upper()}.")
--- Crash_Course/hello.py
+++ Crash_Course/hello.py
@@ -1,1 +1,4 @@
-print("Hello world")
+
+#!/Users/nabinniroula/anaconda3/bin/python
+
+print("hello world".title()) # title method makes first letter of each word upper case
