b1d79a028c9f08077ece81090aa3649e89f90d55
Nabin <nkbhaie2017@gmail.com>
2021-09-20 10:56:09 -0600

    add default parameter arguments in function call

--- Crash_Course/func.py
+++ Crash_Course/func.py
@@ -19,5 +19,21 @@ def name_work(name, work):
     """ uses keyword arguments pair for passing arguments. """
     return f"Hello {name}, I like {work}."
 
-print(name_work(name = "Nabin", work = "Student"))
-print(name_work(work = "Software Engineering", name = "Nbain"))
+# print(name_work(name = "Nabin", work = "Student"))
+# print(name_work(work = "Software Engineering", name = "Nbain"))
+name_work(work = "Customer Service", name = "Nabin")
+
+
+# Default values in functions
+
+def default_values(name, work = "Software Engineering"): # this is called a default value, could be overridden in func call
+    """ uses default parameter values. """
+    return name + ", " + work
+# ways to call this function. 
+#1 withour overriding any values
+no_overriding = default_values("Nabin")
+print(f"Output before overriding the default parameter is {no_overriding}")
+
+with_overriding = default_values(name = "NK", work = "Student")
+print(f"Output after overring the default parameter is {with_overriding}")
+
