9329730eccb4d9b45e93c65b20719d41eb088957
Nabin <nkbhaie2017@gmail.com>
2021-09-19 22:57:26 -0600

    use module operator to find if a number is odd or even

---
+++ Crash_Course/general.py
@@ -0,0 +1,9 @@
+#!/Users/nabinniroula/anaconda3/bin/python
+
+# chekc if a number is odd or an even number
+usser_input = int(input("Please enter your number: "))
+if usser_input % 2 == 0:
+    print("It is an even number.")
+else:
+    print("It is an odd number.")
+
