3350a30a6584785270f1380c8b99086d959872f3
Nabin <nkbhaie2017@gmail.com>
2021-09-20 08:51:22 -0600

    add while loop

--- Crash_Course/general.py
+++ Crash_Course/general.py
@@ -1,9 +1,40 @@
 #!/Users/nabinniroula/anaconda3/bin/python
 
 # chekc if a number is odd or an even number
-usser_input = int(input("Please enter your number: "))
+
+""" 
+usser_input = int(input("Please enter your number: "))  # int() means for number
+
 if usser_input % 2 == 0:
     print("It is an even number.")
-else:
+elif usser_input % 2 == 1:
     print("It is an odd number.")
+# else:
+#     print("Invalid input, please try next time.")
+
+"""
+# While loop
+
+DONE = True
+even_list = []
+while DONE:
+    user_input = input("Please Enter a number: ")
+    if user_input.isdigit():   # isdigit() considers negative numbers as False value, it is a string function.
+        user_input = int(user_input)
+        if user_input > 0 and user_input < 100:
+            # even_list = [i for i in range(user_input) if i%2 == 0]
+            for i in range(user_input + 1):
+                if i % 2 == 0:
+                    even_list.append(i)
+                else:
+                    DONE = False
+            break # break breaks out off the loop and stops execution
+        else:
+            print("invalid input, please try again below...")
+            continue  # continue takes back to the top of the loop
+    else:
+        print("Not a valid input, try again ...")
+        continue
+print(even_list)
+     
 
