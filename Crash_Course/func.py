b4f1c2d177532e7bfac025bfa75c55527a020cb5
Nabin <nkbhaie2017@gmail.com>
2021-09-20 10:22:03 -0600

    add function wokr

---
+++ Crash_Course/func.py
@@ -0,0 +1,7 @@
+
+def get_name():
+    name = input("Enter your name: ")
+    print(f"Hello, {name.title()}!")
+    # this does not simply display anything if you do not call this function.
+# function call
+get_name()
