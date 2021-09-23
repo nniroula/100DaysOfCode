269e8067e558918e8ec8617fab5f073ba4d7c005
nabin <nkbhaie2017@gmail.com>
2021-09-23 16:05:12 -0600

    add an inheritacne structure

--- Crash_Course/inheritance.py
+++ Crash_Course/inheritance.py
@@ -13,6 +13,25 @@ class Car():
         return self.price
 
 car = Car("CRV", "Honda", 2021)
-print(car.make)
-print(car.model)
-print(f"The year of manufacturing is {car.year}.")
+# print(car.make)
+# print(car.model)
+# print(f"The year of manufacturing is {car.year}.")
+car.set_price(35000)
+# print(car.price)
+
+# Inheritance
+
+class ElectricCar(Car):
+    """ This is a child class. We are doing inheritance example here. """
+    def __init__(self, make, model, year, condition):
+        # call parent's constructor
+        super().__init__(make, model, year)
+        self.condition = condition
+
+    def describe_car(self):
+        return f"The instance of an electric car is as follows: \n\t{self.make} {self.model} {self.year} {self.condition}"
+
+ecar = ElectricCar("Chevy", "Traverse", "21", "very good condition")
+# print(f"The make of an electric car is {ecar.make}.")
+call_method = ecar.describe_car()
+print(call_method)
