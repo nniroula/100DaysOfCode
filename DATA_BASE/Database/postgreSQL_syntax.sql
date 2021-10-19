3e48dbbfdff62db93b36952faa59bf8c1d67ceb7
nabin <nkbhaie2017@gmail.com>
2021-10-19 10:12:03 -0600

    add more postgresql syntaxt

--- DATA_BASE/Database/postgreSQL_syntax.sql
+++ DATA_BASE/Database/postgreSQL_syntax.sql
@@ -1,3 +1,7 @@
 --Give the total GDP of Africa
 SELECT SUM(gdp) FROM world
 WHERE continent = 'Africa';
+
+-- Get poplulation of 3 countries
+SELECT SUM(population) FROM world
+WHERE 'Estonia' IN (name) OR 'Latvia' IN (name) OR 'Lithuania' IN (name);
