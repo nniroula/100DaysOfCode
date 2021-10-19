4f9e0e76fc6fb972350fed8b727b173f7921d357
nabin <nkbhaie2017@gmail.com>
2021-10-19 11:00:45 -0600

    add more postgresql syntax

--- DATA_BASE/Database/postgreSQL_syntax.sql
+++ DATA_BASE/Database/postgreSQL_syntax.sql
@@ -5,3 +5,11 @@ WHERE continent = 'Africa';
 -- Get poplulation of 3 countries
 SELECT SUM(population) FROM world
 WHERE 'Estonia' IN (name) OR 'Latvia' IN (name) OR 'Lithuania' IN (name);
+
+
+-- GROUP BY and HAVING. The HAVING clause is tested after the GROUP BY. You can test the aggregated values with 
+-- a HAVING clause. Show the total population of those continents with a total population of at least half a billion.
+SELECT continent, SUM(population)
+    FROM world
+    GROUP BY continent
+    HAVING SUM(population)>500000000
