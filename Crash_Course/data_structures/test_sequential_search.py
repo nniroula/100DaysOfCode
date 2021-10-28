ce74200732ebb71cf0a0bc211c45328f46409ec8
nabin <nkbhaie2017@gmail.com>
2021-10-27 21:55:40 -0600

    add setUp() method

--- Crash_Course/data_structures/test_sequential_search.py
+++ Crash_Course/data_structures/test_sequential_search.py
@@ -4,17 +4,19 @@ from sequential_search import SequentialSearch
 class TestSequentialSearch(unittest.TestCase):
     """ Tests SequentialSearch """
 
+    def setUp(self):
+        self.list1 = [1, 2, 3, 4]
+
     def test_sequential_search(self):
         """ test sequential search function """
         #instantiate the class
-        list1 = [1, 2, 3, 4]
-        inst1 = SequentialSearch(list1, 2)
+        inst1 = SequentialSearch(self.list1, 2)
         result1 = inst1.sequential_search()
         self.assertEqual(True, result1)
 
     def test_not_in_list(self):
-        list1 = [1, 2, 3, 4]
-        inst1 = SequentialSearch(list1, 5)
+        """ test that an item is not in a list """
+        inst1 = SequentialSearch(self.list1, 5)
         result1 = inst1.sequential_search()
         self.assertEqual(False, result1)
 
