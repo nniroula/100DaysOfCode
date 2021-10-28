af7551046f14af3cddb403fef96ac2a817e21dbc
nabin <nkbhaie2017@gmail.com>
2021-10-27 21:45:24 -0600

    add test for sequential search

---
+++ Crash_Course/data_structures/test_sequential_search.py
@@ -0,0 +1,17 @@
+import unittest
+from sequential_search import SequentialSearch
+
+class TestSequentialSearch(unittest.TestCase):
+    """ Tests SequentialSearch """
+
+    def test_sequential_search(self):
+        """ test sequential search function """
+        #instantiate the class
+        list1 = [1, 2, 3, 4]
+        inst1 = SequentialSearch(list1, 2)
+        result1 = inst1.sequential_search()
+        self.assertEqual(True, result1)
+
+
+unittest.main()
+        
