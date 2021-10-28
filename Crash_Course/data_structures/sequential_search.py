e0f52ba05e6a2d9f3657f1b92ea6abb20ee35f78
nabin <nkbhaie2017@gmail.com>
2021-10-27 20:17:14 -0600

    modify sequential search

--- Crash_Course/data_structures/sequential_search.py
+++ Crash_Course/data_structures/sequential_search.py
@@ -2,20 +2,20 @@ class SequentialSearch():
     """ This does sequential search """
 
     def __init__(self, list1, item):
+        """ creates variables needed for the function"""
         self.list1 = list1
         self.item = item
         self.found = False
 
     def sequential_search(self):
+        """ searches if an item is in the list """
         for i in range(0, len(self.list1)):
             if self.list1[i] == self.item:
                 self.found = True
-            else:
-                self.found = False
         return self.found
 
 # instantiate the class
 list1 = [1, 2, 3, 4]
-inst1 = SequentialSearch(list1, 5)
+inst1 = SequentialSearch(list1, 2)
 result1 = inst1.sequential_search()
 print(result1)
