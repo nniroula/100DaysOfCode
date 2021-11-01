7f8fe1cf8e984dad2c1aebfa9378ad43257745fc
nabin <nkbhaie2017@gmail.com>
2021-10-31 22:30:16 -0600

    binary search

--- Crash_Course/data_structures/binary_search.py
+++ Crash_Course/data_structures/binary_search.py
@@ -1,33 +1,34 @@
+class BinarySearch():
+    """ uses binary search algorithm. In binary search you always divide list into two halves until you find an item"""
 
+    def __init__(self, list, val):
+        self.list = list
+        self.val = val
 
-def binary_search(list1, value):
-    found = False
-    if len(list1) == 0:
-        found = False 
-    if len(list1) == 1 and value == list1[0]:
-        found = True
-
-    if len(list1) > 1:
-        while found:
-        # sort the list
-            sorted_list = list1.sort()
-            
-            # find the middle value
-            middle_value = (sorted_list[0] + sorted_list[len(sorted_list) - 1])/2
-            print("Middle value", middle_value)
-            if value == middle_value:
+    def binary_search(self):
+        start_index = 0
+        end_index = len(self.list) - 1
+        found = False
+        mid_index = (start_index + end_index)//2 
+        # while start_index <= end_index:
+        for i in range(start_index, end_index):
+            mid_index = (start_index + end_index)//2 
+            if self.val > self.list[mid_index]:
+                # move start_index to mid_index
+                start_index = mid_index
+                continue
+            if self.val < self.list[mid_index]:
+            #     # modve upper bound to mid_index
+                end_index = mid_index
+                continue
+            if self.val == self.list[mid_index]:
                 found = True
-            elif value > middle_value:
-                new_list = sorted_list[middle_value + 1:]
-                mid_val = (new_list[0] + new_list[len(new_list) - 1])/2
-                if mid_val == value:
-                    found = True
-            elif value < middle_value:
-                new_list = sorted_list[0: middle_value]
-                mid_val = (new_list[0] + new_list[len(new_list) - 1])/2
-                if mid_val == value:
-                    found = True
-    # return found
-a = [2, 4]
-result = binary_search(a, 2)
-print(result)
+            #     return found
+                # break
+        return found
+
+a = [2, 4, 6, 8, 10]
+obj1 = BinarySearch(a, 10)
+output = obj1.binary_search()
+print(output)
+
