74cf59e8ab6591654a3f230192911263b2b6c0ec
nabin <nkbhaie2017@gmail.com>
2021-11-08 20:35:59 -0700

    add one more instance of a class to check if a number is a prime

--- Crash_Course/data_structures/Problem_solving_skills/prime_numbers.py
+++ Crash_Course/data_structures/Problem_solving_skills/prime_numbers.py
@@ -10,3 +10,11 @@ class PrimeNumber():
             if self.number % i == 0:
                 return "It is not a prime number."
         return "It is a PRIME number."
+
+obj1 = PrimeNumber(10)
+result1 = obj1.prime_number()
+print(result1)
+
+obj2 = PrimeNumber(13)
+result2 = obj2.prime_number()
+print(result2)
