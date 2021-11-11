1eebb1e00f58cbbe427ecfa8dcfaf73c1195f8e1
nabin <nkbhaie2017@gmail.com>
2021-11-10 20:39:17 -0700

    fix bubble sort algorithm

--- Crash_Course/data_structures/bubble_sort.py
+++ Crash_Course/data_structures/bubble_sort.py
@@ -7,16 +7,19 @@ class BubbleSort():
 
     def bubble_sort(self):
         """ it defines a bubble sort function."""
-        for i in range (len(self.list1)):
-            if self.list1[i] > self.list1[i + 1]:
-                # swap them out
-                temp = self.list1[i]
-                self.list1[i] = self.list1[i + 1]
-                self.list1[i + 1] = temp
+        #for i in range(len(self.list1) - 1):
+        for i in range(len(self.list1) - 1):
+            for j in range(len(self.list1) - 1):
+                if (self.list1[j] > self.list1[j + 1]):
+                    # swap them out
+                    temp = self.list1[j]
+                    self.list1[j] = self.list1[j + 1]
+                    self.list1[j + 1] = temp
         return self.list1
 
 # instantiate the class
-arr = [ 2, 8, 6, 4, 5, 3, 7]
+# arr = [2, 8, 6, 4, 5, 3, 7, 22, 14, 18, 12, 20]
+arr = ['charles', 'john', 'sarah', 'alex', 'bill']
 obj1 = BubbleSort(arr)
 output1 = obj1.bubble_sort()
 print(output1)
