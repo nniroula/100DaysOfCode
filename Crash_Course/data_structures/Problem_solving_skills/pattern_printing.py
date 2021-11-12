27cfb9518ef108903d3e0f94d271395bcaff1a21
nabin <nkbhaie2017@gmail.com>
2021-11-11 18:25:12 -0700

    object oriented pattern printing

--- Crash_Course/data_structures/Problem_solving_skills/pattern_printing.py
+++ Crash_Course/data_structures/Problem_solving_skills/pattern_printing.py
@@ -5,7 +5,19 @@
 ***
 ****
 """
+class PatternPrinting():
+    """ Prints pattern in certain order."""
 
-for i in range(1, 2):
-    for j in range(1, 5):
-        print('*' * j) 
+    def __init__(self, number):
+        """ get the user's number for pattern printing. """
+        self.number = number
+
+    def print_pattern(self):
+        for i in range(1, 2):
+            for j in range(1, self.number + 1):
+                print('*' * j) 
+
+obj1 = PatternPrinting(5)
+output1 = obj1.print_pattern()
+print(output1)
+        
