a8c3368580147f2099b5b7dc53b693f1623f6d56
nabin <nkbhaie2017@gmail.com>
2021-11-12 21:22:05 -0700

    add a unit test for selection sort algorithm

--- Crash_Course/data_structures/test_selection_sort1.py
+++ Crash_Course/data_structures/test_selection_sort1.py
@@ -11,6 +11,18 @@ class TestSelectionSort1(unittest.TestCase):
         output = obj1.selection_sort()
         self.assertEqual([1, 3, 4, 7, 8, 10], output)
 
+    def test_empty_list(self):
+        list2 = []
+        obj2 = SelectionSort1(list2)
+        output2 = obj2.selection_sort()
+        self.assertEqual([], output2)
+
+    def test_one_element_list(self):
+        list3 = [1]
+        obj3 = SelectionSort1(list3)
+        output3 = obj3.selection_sort()
+        self.assertEqual([1], output3)
+
 unittest.main()
         
     
