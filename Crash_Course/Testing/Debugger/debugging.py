aa10dad17f3e74e6d649b79392d32dc5b2ef03f1
nabin <nkbhaie2017@gmail.com>
2021-12-16 06:57:53 -0700

    add instructions on how to run pdb in terminal

--- Crash_Course/Testing/Debugger/debugging.py
+++ Crash_Course/Testing/Debugger/debugging.py
@@ -14,3 +14,13 @@ if __name__ == "__main__":
 # When you run the codebase, pdb is shown like a virtual env terminal only when the code reaches that execution, and
 # then stops at that point. Do help and hit enter- it shows all commands. help command_name gives info about that command
 # q or quit to get out of the pdb
+
+"""
+To run a debugger(pdb):
+one way: python3 -m pdb filename.py
+second way: in terminal => import pdb
+                            pdb.set_trace()
+third way: in the line where you wanna stop code execution, do this
+            import pbd; pdb.set_trace()
+            
+"""
